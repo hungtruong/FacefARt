c69cbb83530aca0c8f338d025770010f0f1d3001
Hung Truong <hung@hung-truong.com>
2017-11-04 17:34:07 -0700

    Initial commit

--- FacefARt/ViewController.swift
+++ FacefARt/ViewController.swift
@@ -7,19 +7,101 @@
 //
 
 import UIKit
+import SceneKit
+import ARKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    /// This is the ARSCNView that we use to get the AR session and face anchor data from
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    /// This is the fart sound player. It gets loaded on viewDidLoad and replaced for each new fart play
+    var fartPlayer : AVAudioPlayer?
+    
+    /// Tracks when the mouth has opened and when it's closed
+    var mouthOpen : Bool = false
+    
+    /// The data asset for the fart sound, reusing it for efficiency.
+    lazy var fartDataAsset : NSDataAsset? = {
+        return NSDataAsset(name: "Fart")
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // If Face Tracking isn't supported then just return
+        guard ARFaceTrackingConfiguration.isSupported else { return }
+        
+        // Set up a face tracking configuration and then start a session with it.
+        let configuration = ARFaceTrackingConfiguration()
+        sceneView.delegate  = self
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
+        //Pretty self explanatory. Sets up the fart sound player for the first fart.
+        prepareToFart()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    /// Delegate method that updates the ARFaceAnchor data as it comes in thru the session.
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+        guard let faceAnchor = anchor as? ARFaceAnchor else { return }
+        
+        // There are a bunch of faceAnchor properties but we only care about the mouth opening for this app.
+        // Strangely, its' called "mouthClose" but 0 is closed and 1 is open... Shouldn't it be the other way around?
+        if let mouthOpenness = faceAnchor.blendShapes[ARFaceAnchor.BlendShapeLocation.mouthClose]?.doubleValue {
+            // The value given here ranges from really low to somewhat low, not sure if it could ever get to 1.0
+            // So 0.045 seems like a decent number to catch the open mouth but miss some false positives /shrug
+            if mouthOpenness > 0.045 {
+                handleMouthOpen()
+            } else {
+                handleMouthClosed()
+            }
+        }
+    }
 
+    /// Handle the mouth opening, if the mouth is already open then just return early
+    func handleMouthOpen() {
+        guard !mouthOpen else { return }
+        mouthOpen = true
+        startFarting()
+    }
+    
+    /// Opposite of the mouth opening func
+    func handleMouthClosed() {
+        guard mouthOpen else { return }
+        mouthOpen = false
+        stopFarting()
+    }
+    
+    /// This just gets the fart player ready. I guess if I didn't do this the fart sound might be a little delayed. Gotta get those fart buffers ready.
+    func prepareToFart() {
+        guard let fartDataAsset = fartDataAsset else { return }
+        do {
+        fartPlayer = try AVAudioPlayer(data:fartDataAsset.data, fileTypeHint:"mp3")
+        fartPlayer?.prepareToPlay()
+        } catch {
+            
+        }
+    }
+    
+    /// Starts the fart player's sound (which is a fart sound)
+    func startFarting() {
+        guard let fartPlayer = fartPlayer else { return }
+        fartPlayer.play()
+    }
+    
+    /// If the person closes their mouth, the fart sound stops prematurely, then gets another fart player ready.
+    func stopFarting() {
+        guard let fartPlayer = fartPlayer else { return }
+        fartPlayer.stop()
+        prepareToFart()
+    }
+    
 }
 
---
+++ README.md
@@ -0,0 +1,36 @@
+# FacefARt
+
+FacefARt is an app for the iPhone X that demonstrates the potential of the TrueDepth front-facing camera and ARKit to make the user's face fart when they open their mouth.
+
+## Getting Started
+
+A passion for fart apps is the only thing you need to get started.
+
+### Prerequisites
+
+I guess you'll also need an iPhone X and Xcode 9, and the ability to load a debug version of the app onto the device.
+
+## How it works
+
+The FacefARt app works by beginning an ARKit session, running the ARSCNView's session with a ARFaceTrackingConfiguration config. The main view controller is set as the ARSCNView's delegate, which calls the `renderer(_:didUpdate:for:)` method, passing along an `ARFaceAnchor` which contains information about the various anchor points on the user's face.
+
+We only care about the `ARFaceAnchor.BlendShapeLocation.mouthClose` value which gives us the "openness" of the user's mouth. When the user opens their mouth, we start playing a fart sound. When we get an update with the `mouthClose` value such that we detect the user has closed their mouth, we stop the fart sound. It's a really long fart sound so if you leave your mouth open it'll fart for a while.
+
+## Further Reading
+
+* [Creating Face-Based AR Experiences](https://developer.apple.com/documentation/arkit/creating_face_based_ar_experiences) - Apple's sample code on how to use the ARKit Face stuff.
+* [Maven](https://maven.apache.org/) - Dependency Management
+* [ROME](https://rometools.github.io/rome/) - Used to generate RSS Feeds
+
+## Authors
+
+* **Hung Truong** - [hungtruong](https://github.com/hungtruong)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Thanks to Apple for not canceling my developer license
+* Also thanks to Apple for providing some really good sample code
